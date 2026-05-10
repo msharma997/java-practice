dd1f7cf7d9f2f243bbeb824dea844a84b0239dd3
Mansi <manimansi161@gmail.com>
2026-05-10 22:01:27 +0530

    Add files via upload

---
+++ FloatToInt.java
@@ -0,0 +1,8 @@
+public class FloatToInt {
+    public static void main(String[]args) {
+        float num = 12.78f;
+        int result = (int) num;
+        System.out.println("Float value: " + num);
+        System.out.println("Converted integer: " + result);
+    }
+}
